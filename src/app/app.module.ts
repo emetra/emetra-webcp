6f582b49c85a5ba399fe20a8bdca79ca1edc3a30
kamuishirou33 <handywijaya33@gmail.com>
2017-10-22 03:43:30 +0700

    Navbar Routing and About

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 import { BsDropdownModule, CollapseModule } from 'ngx-bootstrap';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContactComponent,
+    AboutComponent,
+    ServicesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    FormsModule,
     BsDropdownModule.forRoot(),
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'service',
+        component: ServicesComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
