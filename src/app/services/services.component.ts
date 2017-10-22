780f512ec7822791b62f5c3561e5bb16045b2095
kamuishirou33 <handywijaya33@gmail.com>
2017-10-22 23:41:33 +0700

    editing services

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'em-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent implements OnInit {
 
